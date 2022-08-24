4b578e81dc17608b29f5ebb697dbe895646fbb6c
zoevds <zoevanderschyff13@gmail.com>
2022-08-24 14:39:48 +0200

    npm

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // const cors = require("cors"); // Used to prevent errors when working locally
 // const app = express(); // Initialize express as an app variable
 
-// const productRoute = require("./Routes/productRoute");
-// const userRoute = require("./Routes/userRoute");
-// const categoriesRoute = require("./Routes/categoriesRoute");
-// // const flavoursRoute = require("./Routes/flavoursRoute");
+const productRoute = require("./Routes/productRoute");
+const userRoute = require("./Routes/userRoute");
+const categoriesRoute = require("./Routes/categoriesRoute");
+const flavoursRoute = require("./Routes/flavoursRoute");
 
 // app.set("port", process.env.PORT || 3256); // Set the port
 // app.use(express.json()); // Enable the server to handle JSON requests
@@ -18,13 +18,13 @@
 //   res.sendFile(__dirname + "/" + "index.html");
 // });
 
-// app.use("/productRoute", productRoute);
-// app.use("/userRoute", userRoute);
-// app.use("/categories", categoriesRoute);
-// // app.use("/flavours", flavoursRoute);
-// app.listen(app.get("port"), () => {
-//   console.log("server running");
-// });
+app.use("/productRoute", productRoute);
+app.use("/userRoute", userRoute);
+app.use("/categories", categoriesRoute);
+app.use("/flavours", flavoursRoute);
+app.listen(app.get("port"), () => {
+  console.log("server running");
+});
 
 // app.listen(app.get("port"), () => {
 //   console.log(`Listening for calls on port ${app.get("port")}`);
@@ -42,10 +42,10 @@ app.use("/index.html", express.static(__dirname + "/index.html"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/" + "index.html");
 });
-app.use("/users", require("./routes/userRoute"));
-app.use("/products", require("./routes/productRoute"));
-app.use("/categories", require("./routes/categoriesRoute"));
-app.use("/flavours", require("./routes/flavoursRoute"));
+// app.use("/users", require("./routes/userRoute"));
+// app.use("/products", require("./routes/productRoute"));
+// app.use("/categories", require("./routes/categoriesRoute"));
+// app.use("/flavours", require("./routes/flavoursRoute"));
 app.listen(app.get("port"), () => {
   console.log(`Listening for calls on port ${app.get("port")}`);
   console.log("Press Ctrl+C to exit server");
