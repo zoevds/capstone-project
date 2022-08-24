a6ccdf708c3cf44b71ecb97a4ed9dee052d93f21
zoevds <zoevanderschyff13@gmail.com>
2022-08-24 15:34:42 +0200

    Heroku test

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-// const express = require("express"); // Used to set up a server
-// const cors = require("cors"); // Used to prevent errors when working locally
-// const app = express(); // Initialize express as an app variable
-
+const express = require("express"); // Used to set up a server
+const cors = require("cors"); // Used to prevent errors when working locally
+const app = express(); // Initialize express as an app variable
+require("dotenv").config();
 const productRoute = require("./Routes/productRoute");
 const userRoute = require("./Routes/userRoute");
 const categoriesRoute = require("./Routes/categoriesRoute");
 const flavoursRoute = require("./Routes/flavoursRoute");
 
-// app.set("port", process.env.PORT || 3256); // Set the port
-// app.use(express.json()); // Enable the server to handle JSON requests
-// app.use(cors()); // Dont let local development give errors
-// app.use(express.static("public"));
-// //Serves all the request which includes /images in the url from Images folder
-// app.use("/index.html", express.static(__dirname + "/index.html"));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/" + "index.html");
-// });
-
-// app.use("/product", productRoute);
-app.use("/user", userRoute);
-app.use("/categories", categoriesRoute);
-app.use("/flavours", flavoursRoute);
-app.listen(app.get("port"), () => {
-  console.log("server running");
-});
-
-// app.listen(app.get("port"), () => {
-//   console.log(`Listening for calls on port ${app.get("port")}`);
-//   console.log("Press Ctrl+C to exit server");
-// });
-const express = require("express"); // Used to set up a server
-const cors = require("cors"); // Used to prevent errors when working locally
-const app = express(); // Initialize express as an app variable
-app.set("port", process.env.PORT || 4427); // Set the port
+app.set("port", process.env.PORT || 3000); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
 app.use(express.static("public"));
@@ -42,10 +16,11 @@ app.use("/index.html", express.static(__dirname + "/index.html"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/" + "index.html");
 });
-// app.use("/users", require("./routes/userRoute"));
-// app.use("/products", require("./routes/productRoute"));
-// app.use("/categories", require("./routes/categoriesRoute"));
-// app.use("/flavours", require("./routes/flavoursRoute"));
+
+app.use("/product", productRoute);
+app.use("/user", userRoute);
+app.use("/categories", categoriesRoute);
+app.use("/flavours", flavoursRoute);
 app.listen(app.get("port"), () => {
   console.log(`Listening for calls on port ${app.get("port")}`);
   console.log("Press Ctrl+C to exit server");
